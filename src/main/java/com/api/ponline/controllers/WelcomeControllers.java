b5b9fbccdb6f37cc0459eceee53d7806b4e46595
Fredy Hermawan <fredihermawan1211@gmail.com>
2022-10-13 10:42:29 +0700

    SETUP PROJECT BE

---
+++ src/main/java/com/api/ponline/controllers/WelcomeControllers.java
@@ -0,0 +1,16 @@
+package com.api.ponline.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeControllers {
+    
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Rest Api SIHAN";
+    }
+    
+}

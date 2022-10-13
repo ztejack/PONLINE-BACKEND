61f188483906cf99f33cf301759ac958cf886acd
Fredy Hermawan <fredihermawan1211@gmail.com>
2022-10-13 10:24:44 +0700

    SETUP PROJECT BE

--- src/main/java/com/api/ponline/ApiPonlineApplication.java
+++ src/main/java/com/api/ponline/ApiPonlineApplication.java
@@ -1,4 +1,4 @@
-package com.api.ponline.apiponline;
+package com.api.ponline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

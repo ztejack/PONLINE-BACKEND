25e8f1b7bb799955d13ef6e98990d36601f2bf36
Fredy Hermawan <fredihermawan1211@gmail.com>
2022-10-13 10:20:59 +0700

    SETUP PROJECT BACKEND

---
+++ src/main/java/com/api/ponline/apiponline/ApiPonlineApplication.java
@@ -0,0 +1,13 @@
+package com.api.ponline.apiponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ponline/apiponline/ApiPonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ponline.apiponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
